8e0c3323f4ca2b00bbb4d07fc291ccbde999944c
Wei De Su <weide_su@mitake.com.tw>
2021-04-08 18:08:52 +0800

    Made example code as test-case

--- src/main/java/tw/com/wd/example/rocksdb/Executor.java
+++ src/main/java/tw/com/wd/example/rocksdb/Executor.java
@@ -4,11 +4,5 @@ package tw.com.wd.example.rocksdb;
 public class Executor {
     public static void main(String[] args) {
         System.out.printf("Hello RocksDB.\n");
-
-
-
-        ExDBOpen exDBOpen = new ExDBOpen(100);
-        //exDBOpen.caseOpenOneTime();
-        exDBOpen.caseOpenEveryTime();
     }
 }
